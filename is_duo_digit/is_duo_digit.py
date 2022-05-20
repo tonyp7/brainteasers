d5177786b16df30ddd423123a71773fca0fc0656
tonyp7 <bubbathemaster@gmail.com>
2022-05-20 11:51:05 +0800

    added perf

--- is_duo_digit/is_duo_digit.py
+++ is_duo_digit/is_duo_digit.py
@@ -25,9 +25,41 @@ def is_duo_digit(number):
 
 
 
+def is_duo_digit_performance(number):
+    #transform the number to its string representation, without the negative sign if any
+    str_n = str(abs(number))
+
+    d1 = ''
+    d2 = ''
+    for c in str_n:
+
+        #first
+        if d1 == '':
+            d1 = c
+        #initialize d2 to another digit found that is not d1
+        elif d2 == '' and d1 != c:
+            d2 = c
+        #check if we have detected a 3rd digit
+        elif c != d1 and c != d2: #not a duo_digit, break as soon as possible
+            return 'n'
+
+    return 'y'
+
+
+
 #tests
+print('is_duo_digit')
 print(is_duo_digit(12))
 print(is_duo_digit(-3333))
 print(is_duo_digit(102))
 print(is_duo_digit(789))
 print(is_duo_digit(7887))
+
+print('is_duo_digit_performance')
+print(is_duo_digit_performance(12))
+print(is_duo_digit_performance(-3333))
+print(is_duo_digit_performance(102))
+print(is_duo_digit_performance(789))
+print(is_duo_digit_performance(7887))
+
+
